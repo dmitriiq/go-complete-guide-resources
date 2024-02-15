7a4303058c4f39c6865ae091d2a5e94e2aae50ea
Dmitrii Baikov <dmitrii.baikov.q@gmail.com>
2024-02-15 22:08:25 +0100

    Execise 116

--- code/07-arrays-slices-maps/06-exercise-problem/practice.go
+++ code/07-arrays-slices-maps/06-exercise-problem/practice.go
@@ -1,23 +1,59 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	// Time to practice what you learned!
+
+	// 1) Create a new array (!) that contains three hobbies you have
+	// 		Output (print) that array in the command line.
+
+	hobbies := []string{"reading", "films", "musims"}
+	fmt.Println(hobbies)
+
+	// 2) Also output more data about that array:
+	//		- The first element (standalone)
+	//		- The second and third element combined as a new list
+	fmt.Println(hobbies[0])
+	fmt.Println([]string{hobbies[1], hobbies[2]})
+	// 3) Create a slice based on the first element that contains
+	//		the first and second elements.
+	//		Create that slice in two different ways (i.e. create two slices in the end)
+	fmt.Println(hobbies[:2])
+	fmt.Println(hobbies[1:], hobbies[2:])
+	// 4) Re-slice the slice from (3) and change it to contain the second
+	//		and last element of the original array.
+
+	// 5) Create a "dynamic array" that contains your course goals (at least 2 goals)
+	// 6) Set the second goal to a different one AND then add a third goal to that existing dynamic array
+	// 7) Bonus: Create a "Product" struct with title, id, price and create a
+	//		dynamic list of products (at least 2 products).
+	//		Then add a third product to the existing list of products.
+	type Product struct {
+		title string
+		id    string
+		price float64
+	}
+
+	productList := []Product{
+		{
+			title: "Eis",
+			id:    "ies01",
+			price: 8.88,
+		},
+		{
+			title: "Gemuese",
+			id:    "gemuese01",
+			price: 9.11,
+		},
+	}
+	fmt.Println(productList)
+	productList = append(productList, Product{
+		title: "Saft",
+		id:    "saft01",
+		price: 3.44,
+	})
+	fmt.Println(productList)
+
 }
-
-// Time to practice what you learned!
-
-// 1) Create a new array (!) that contains three hobbies you have
-// 		Output (print) that array in the command line.
-// 2) Also output more data about that array:
-//		- The first element (standalone)
-//		- The second and third element combined as a new list
-// 3) Create a slice based on the first element that contains
-//		the first and second elements.
-//		Create that slice in two different ways (i.e. create two slices in the end)
-// 4) Re-slice the slice from (3) and change it to contain the second
-//		and last element of the original array.
-// 5) Create a "dynamic array" that contains your course goals (at least 2 goals)
-// 6) Set the second goal to a different one AND then add a third goal to that existing dynamic array
-// 7) Bonus: Create a "Product" struct with title, id, price and create a
-//		dynamic list of products (at least 2 products).
-//		Then add a third product to the existing list of products.
